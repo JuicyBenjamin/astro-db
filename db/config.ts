20a41c3272c62e143e973f6fee6a083eef60405a
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-04-12 16:33:57 +0200

    feat: add comments to test page

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,22 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db'
+
+const Author = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+  },
+})
+
+const Comment = defineTable({
+  columns: {
+    authorId: column.text({ references: () => Author.columns.id }),
+    body: column.text(),
+  },
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
-});
+  tables: {
+    Author,
+    Comment,
+  },
+})
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { db, Comment, Author } from 'astro:db'
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Author).values([{ id: '1' }, { id: '2' }])
+  await db.insert(Comment).values([
+    {
+      authorId: '1',
+      body: 'This is a comment',
+    },
+    {
+      authorId: '2',
+      body: 'This is another comment',
+    },
+  ])
 }
